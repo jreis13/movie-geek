32b17e3ee8ba02afc31bb626cd5a45260907d906
João Francisco Reis <joaofranciscoreis13@outlook.com>
2023-07-25 12:47:18 +0100

    Remove unused type

--- src/utilities/types/MovieSearchResultType.ts
+++
@@ -1,9 +0,0 @@
-import MovieType from "./MovieType";
-
-interface MovieSearchResultType {
-  Search: MovieType[];
-  totalResults: string;
-  Response: string;
-}
-
-export default MovieSearchResultType;
